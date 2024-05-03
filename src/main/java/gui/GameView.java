b43aa77a8ae77c71f0020b82e698b0ea7fc8df7a
LOTHSAVAN hugo <hugo.lothsavan@etu.u-paris.fr>
2024-05-03 19:29:32 +0200

    maj bg

--- src/main/java/gui/GameView.java
+++ src/main/java/gui/GameView.java
@@ -52,7 +52,7 @@ public class GameView extends JPanel{
         //this.knife = game.knife1;
         //this.cible = new Cible("Cible", 100, KnifeDisplay.getBgImgWidth() / 2, KnifeDisplay.getBgImgHeight() / 2, 0);
         if (isSolo) {
-            this.entityDisplay = new EntityDisplay(game.knife1, "src/main/ressources/background/bgMononoke.gif", (ArrayList<Cible>) game.getListeCible(),isSolo);
+            this.entityDisplay = new EntityDisplay(game.knife1, "src/main/ressources/background/bgPokemon2.gif", (ArrayList<Cible>) game.getListeCible(),isSolo);
         }
         else{
             this.entityDisplay = new EntityDisplay(game.knife1, "src/main/ressources/background/fond1v1.jpg", (ArrayList<Cible>) game.getListeCible(),isSolo);
