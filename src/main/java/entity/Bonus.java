f4cb18647093b1b002f96adb9a73cde60cb87b9a
coulombe <corentin.coulombel@etu.u-paris.fr>
2024-05-05 14:06:50 +0200

    ajout de GestionBonus.java pour gérer les effets des bonus

--- src/main/java/entity/Bonus.java
+++ src/main/java/entity/Bonus.java
@@ -1,11 +1,27 @@
 package entity;
 
+import config.Game;
+
 import static entity.TypeCible.CIBLE_BONUS;
 
 public class Bonus extends Cible{
 
-    public Bonus(double x,double y){
+    TypeBonus typeBonnus;
+    Game game;
+
+    public Bonus(double x,double y,TypeBonus typeBonnus){
         super (CIBLE_BONUS,x,y);
+        this.typeBonnus = typeBonnus;
+    }
+
+
+    public enum TypeBonus{
+        BONUS_GEL,
+        BONUS_POWER,        //traverse les cibles
+        BONUS_GOLD,
+        BONUS_XP,
+        BONUS_TRAJ;         //change la trajectoire pour pouvoir toucher plusieurs cibles (traj circulaire)
+
     }
 
 }
